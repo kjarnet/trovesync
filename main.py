28ea330cc1a53ad14538ccb70c49af19795a99a1
kjarnet <kjarnet@gmail.com>
2013-02-13 13:18:47 +0100

    GH-1: Filter on album from user input.

--- main.py
+++ main.py
@@ -3,22 +3,56 @@ import json
 import hashlib
 from os import listdir
 from os.path import isfile, join
+
+# Trovebox service endpoints:
+PHOTOS_LIST = "/photos/list.json"
+ALBUMS_LIST = "/albums/list.json"
+
+""" Get Credentials """
 with open("./cred.json") as credfile:
   cred = json.load(credfile)
 print cred
-client = OpenPhoto(cred["host"], cred["consumerKey"], cred["consumerSecret"], cred["token"], cred["tokenSecret"])
-resp = json.loads(client.get('/photos/list.json'))
-message = resp["message"]
-code = resp["code"]
-result = resp["result"]
-print message
-for i in result:
+
+""" Initialize a webservice client """
+albumPath = cred["albumPath"]
+client = OpenPhoto(
+  cred["host"],
+  cred["consumerKey"],
+  cred["consumerSecret"],
+  cred["token"],
+  cred["tokenSecret"]
+  )
+
+""" Get list of remote albums """
+albresp = json.loads(client.get(ALBUMS_LIST)) 
+albmessage = albresp["message"]
+albcode = albresp["code"]
+remoteAlbums = albresp["result"]
+print albmessage
+for a in remoteAlbums:
+  print a["id"]
+  print a["name"]
+
+""" Choose an album to synchronize """
+albumId = raw_input("Enter id-number of album to synchronize against the folder " + 
+  albumPath + ": ")
+
+""" Get list of remote images """
+imgresp = json.loads(client.get(PHOTOS_LIST))
+imgmessage = imgresp["message"]
+imgcode = imgresp["code"]
+imgresult = imgresp["result"]
+remoteImgs = [i for i in imgresult if albumId in i["albums"]]
+# remoteImgs = imgresult
+print imgmessage
+for i in remoteImgs:
   print i["albums"]
   print i["filenameOriginal"]
   print i["hash"]
+print "Count: " + str(len(remoteImgs))
 
-for f in listdir(cred["albumPath"]):
-  fullfile = join(cred["albumPath"], f)
+for f in listdir(albumPath):
+  fullfile = join(albumPath, f)
   print fullfile
   with open(fullfile, "rb") as imgFile:
     sha = hashlib.sha1(imgFile.read()).hexdigest()
