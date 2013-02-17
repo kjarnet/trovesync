7bb34600ac16fef7729d8a8c572ae971aa310fe5
kjarnet <kjarnet@gmail.com>
2013-02-17 18:23:59 +0100

    GH-1: Implemented sync from remote, but buggy!

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from openphoto import OpenPhoto
 import json
 import hashlib
-from os import listdir
-from os.path import isfile, join
+from os import listdir, path
+import shutil
+import urllib2
+import oauth2 as oauth
 
 # Trovebox service endpoints:
 PHOTOS_LIST = "/photos/list.json"
@@ -16,7 +18,7 @@ print "cred.json: ", cred
 """ Initialize a webservice client """
 albumPath = cred["albumPath"]
 pageSize = cred["maxPhotos"]
-client = OpenPhoto(
+troveboxClient = OpenPhoto(
   cred["host"],
   cred["consumerKey"],
   cred["consumerSecret"],
@@ -25,7 +27,7 @@ client = OpenPhoto(
   )
 
 """ Get list of remote albums """
-albresp = json.loads(client.get(ALBUMS_LIST)) 
+albresp = json.loads(troveboxClient.get(ALBUMS_LIST)) 
 albmessage = albresp["message"]
 albcode = albresp["code"]
 remoteAlbums = albresp["result"]
@@ -44,7 +46,7 @@ while albumId not in ids:
   albumId = raw_input("Please enter a valid album id " + str(ids) + ": ")
 
 """ Get list of remote images """
-imgresp = json.loads(client.get(PHOTOS_LIST, {"pageSize": pageSize}))
+imgresp = json.loads(troveboxClient.get(PHOTOS_LIST, {"pageSize": pageSize}))
 imgmessage = imgresp["message"]
 imgcode = imgresp["code"]
 imgresult = imgresp["result"]
@@ -62,7 +64,8 @@ if len(remoteImgs) >= pageSize:
 """ Loop through local files and compare against remote images """
 localonly = []
 for f in listdir(albumPath):
-  fullfile = join(albumPath, f)
+  fullfile = path.join(albumPath, f)
+  if not path.isfile(fullfile): continue
   print fullfile
   with open(fullfile, "rb") as imgFile:
     sha = hashlib.sha1(imgFile.read()).hexdigest()
@@ -81,17 +84,66 @@ remoteonly = [i for i in remoteImgs if "inSync" not in i]
 print "Local only: ", localonly
 print "Remote only: ", remoteonly
 
+
+
+""" Download file (copied from stackoverflow q. 22676) """
+def download(url, file_name, file_size):
+  print "saving as", file_name
+  " First, build headers using oauth2 (copied from http://pastebin.com/d5n447wq)"
+  parameters = None
+  consumer = oauth.Consumer(troveboxClient.consumer_key, troveboxClient.consumer_secret)
+  access_token = oauth.Token(troveboxClient.token, troveboxClient.token_secret)
+  sig_method = oauth.SignatureMethod_HMAC_SHA1()
+
+  oauth_request = oauth.Request.from_consumer_and_token(
+          consumer, token=access_token, http_method="GET", http_url=url, parameters=parameters
+      )
+  oauth_request.sign_request(sig_method, consumer, access_token)
+  headers = oauth_request.to_header()
+  headers['User-Agent'] = 'Trovesync'
+
+  u = urllib2.urlopen(urllib2.Request(url, headers=headers))
+  f = open(file_name, 'wb')
+  meta = u.info()
+  print meta
+  print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+  file_size_dl = 0
+  block_sz = 8192
+  while True:
+      buffer = u.read(block_sz)
+      if not buffer:
+          break
+
+      file_size_dl += len(buffer)
+      f.write(buffer)
+      status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+      status = status + chr(8)*(len(status)+1)
+      print status
+
+  f.close()
+
+
+
 """ Choose sync direction """
 direction = raw_input("Do you want to sync [r]emote changes to local folder, [l]ocal changes to remote album or [c]hoose for each picture [r/l/c]? ")
 while direction not in ["r", "l", "c"]:
   direction = raw_input("Please choose r for remote, l for local or c for custom: ")
 
+
+
+
+
+backupPath = cred["backupPath"]
 """ Synchronize! """
 if direction == "r":
   for i in localonly:
-    print "move local image to backup location", i
+    fullfile = path.join(albumPath, f)
+    print "move local image ", fullfile, "to backup location", backupPath
+    #shutil.move(fullfile, backupPath)
   for i in remoteonly:
     print "download image", i["filenameOriginal"]
+    download(i["pathDownload"], path.join(albumPath, i["filenameOriginal"]), int(i["size"])*1024)
 elif direction == "l":
   for i in localonly:
     print "upload to remote", i
