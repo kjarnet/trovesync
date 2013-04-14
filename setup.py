38be0993c79dca7f8ed58e0ecee1c7be6170effc
kjarnet <kjarnet@gmail.com>
2013-04-14 18:00:17 +0200

    GH-13: Added files required to use travis ci.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md') as f:
+    long_desc = f.read().splitlines()
+    
+setup(
+    name='trovesync',
+    version='0.1.0',
+    author='Oeystein Kjaernet',
+    author_email='kjarnet on gmail',
+    packages=['trovesync'],
+    url='http://github.com/kjarnet/trovesync',
+    license='LICENSE.txt',
+    description='Application for synchronizing a local folder with a trovebox.com album.',
+    long_description=long_desc,
+    install_requires=required
+)
+
